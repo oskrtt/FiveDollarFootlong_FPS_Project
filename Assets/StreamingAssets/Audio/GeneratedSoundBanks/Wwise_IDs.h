612f11c2927b65923c2d68a9677cc98079fb5e7e
Heath Henry <10979789@uvu.edu>
2026-04-08 15:26:20 -0600

    HoverBot Alerted, Destroyed, Shoot Sounds (Wwise)

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,10 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ENEMY_HOVERBOT_ALERTED = 212203887U;
+        static const AkUniqueID ENEMY_HOVERBOT_DESTROY = 2865467360U;
         static const AkUniqueID ENEMY_HOVERBOT_HURT = 4067705073U;
+        static const AkUniqueID ENEMY_HOVERBOT_SHOOT = 285818559U;
         static const AkUniqueID PLAYER_DEATH = 3083087645U;
         static const AkUniqueID PLAYER_HEAL = 780312891U;
         static const AkUniqueID PLAYER_HURT = 1068092414U;
