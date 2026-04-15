63575998369ed22e51c8f2fbab1e3296f44ee035
BrighamAsman <10970577@uvu.edu>
2026-04-15 15:37:14 -0600

    Added Heartbeat sound, parameter, RTPC, PlayerLife States, and random container for FS

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -21,10 +21,33 @@ namespace AK
         static const AkUniqueID ENEMY_TURRET_SHOOT = 3153550024U;
         static const AkUniqueID LAUNCHER_PICKUP = 3783202364U;
         static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID PLAYER_FS = 4276063004U;
         static const AkUniqueID PLAYER_HEAL = 780312891U;
+        static const AkUniqueID PLAYER_HEARTBEAT = 3484321931U;
         static const AkUniqueID PLAYER_HURT = 1068092414U;
         static const AkUniqueID PLAYER_JUMP = 1305133589U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYERLIFE
+        {
+            static const AkUniqueID GROUP = 444815956U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYERLIFE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYERHEALTH = 151362964U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
