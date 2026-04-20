10fd49a2d023b656c1cb9bd2d654b3d88282719f
oskrtt <joscarrobin28@gmail.com>
2026-04-20 14:59:50 -0600

    Added events for music in Wwise
    i'm trying sumn

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -7,9 +7,8 @@ public class MusicManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playMusic.Post(gameObject);
         AkSoundEngine.SetState("Music", "Level");
-      
+        playMusic.Post(gameObject);
     }
 
 }
